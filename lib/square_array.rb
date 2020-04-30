45450a28275380ca9049b4ffc533dead0e93ee16
Jonathan Reid <iamjonathanreid@gmail.com>
2020-04-30 08:40:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   result = []
   count = 0
   while count < array.size do
-    squared << (array[count] ** 2)
+    result << (array[count] ** 2)
     count = count + 1
   end
   result
